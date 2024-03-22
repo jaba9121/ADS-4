d11be21b084e4fd65c84268cb631d674b4254615
jaba9121 <160114001+jaba9121@users.noreply.github.com>
2024-03-22 12:19:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,29 +5,27 @@ int binSearch(int* arr, int len, int value) {
     int right = len - 1;
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        if (arr[mid] > value) {
-            right = mid - 1;
-        } else {
-            if (arr[mid] < value) {
-                left = mid + 1;
-            } else {
+        if (arr[mid] == value) {
+            count++;
+            int left = mid - 1;
+            int right = mid + 1;
+            while (left >= 0 && arr[left] == value) {
                 count++;
-                int left = mid - 1;
-                int right = mid + 1;
-                while ((left >= 0) && (arr[left] == value)) {
-                    count++;
-                    left--;
-                }
-                while ((right < len) && (arr[right] == value)) {
-                    count++;
-                    right++;
-                }
-                return count;
+                left--;
             }
+            while (right < len && arr[right] == value) {
+                count++;
+                right++;
+            }
+            return count;
         }
-    return count;
+        if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-    return 0;
+    return count;
 }
 int countPairs1(int* arr, int len, int value) {
     int count = 0;
